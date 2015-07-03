e329e31410a66203bdae2fdc8ca654cdc7671b91
Abhishek Angne <abhishek.angne@gmail.com>
2015-07-02 21:11:04 -0400

    Created new Single View application project for FlickerCollectionViewController

---
+++ FlickerCollectionViewController/FlickerCollectionViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlickerCollectionViewController
+//
+//  Created by Abhishek Angne on 7/2/15.
+//  Copyright © 2015 Abhishek Angne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
